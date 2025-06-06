0a2979f59c479cd6c9c1850ac5cb996e9ea2d9fd
minu2004 <minakshibadiger9@gmail.com>
2025-06-06 08:23:32 +0530

    initial commit

---
+++ prg2/src/main/java/prachi/prg2/sharma.java
@@ -0,0 +1,10 @@
+package prachi.prg2;
+
+public class sharma {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+
+	}
+
+}
---
+++ prog2/src/main/java/prachi/prog2/hell.java
@@ -0,0 +1,10 @@
+package prachi.prog2;
+
+public class hell {
+
+	public static void main(String[] args) {
+		System.out.println("hello wolrd");
+
+	}
+
+}
